f4e2a131030584f11b23d84fa558fc788d60d18e
Sampo Syrjänen <shs@iki.fi>
2017-07-19 01:47:09 +0300

    Add channel editor.

--- src/net/kelloradio/app/MainActivity.java
+++ src/net/kelloradio/app/MainActivity.java
@@ -8,6 +8,8 @@ import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.widget.TextView;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.EditText;
 import android.view.View;
 import android.view.ViewGroup;
 import java.util.ArrayList;
@@ -79,6 +81,7 @@ class Player extends Object
             mediaPlayer.setDataSource(this.url);
             mediaPlayer.prepareAsync();
         } catch (Exception e) {
+            error = 1;
             stop();
         }
     }
@@ -145,9 +148,25 @@ public class MainActivity extends Activity
         String name = "";
         String url = "";
         boolean starred = false;
+
+        Channel() {
+        }
+
+        Channel(Channel other) {
+            assign(other);
+        }
+
+        void assign(Channel other) {
+            this.name = other.name;
+            this.url = other.url;
+            this.starred = other.starred;
+        }
     }
 
     ArrayList<Channel> channels = new ArrayList<Channel>();
+    Channel targetChannel = null;
+    Channel editedChannel = new Channel();
+    Channel newChannel = new Channel();
 
     View clickedView = null;
     boolean dirty = false;
@@ -188,6 +207,23 @@ public class MainActivity extends Activity
     public void onSaveInstanceState(Bundle outState) {
         player.saveInstanceState(outState);
         super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onBackPressed() {
+        View main = findViewById(R.id.main_view);
+        View list = findViewById(R.id.channel_list_view);
+        View editor = findViewById(R.id.channel_edit_view);
+        if (main.getVisibility() == View.VISIBLE) {
+            super.onBackPressed();
+        } else if (list.getVisibility() == View.VISIBLE) {
+            changeView(main);
+        } else if (editor.getVisibility() == View.VISIBLE) {
+            changeView(list);
+        } else {
+            changeView(main);
+        }
+        updateView();
     }
 
     public void update() {
@@ -261,13 +297,18 @@ public class MainActivity extends Activity
 
     public void initView() {
         setContentView(R.layout.main);
-        TextView play = (TextView)findViewById(R.id.play);
-        play.setOnClickListener(this);
+        findViewById(R.id.play).setOnClickListener(this);
+        findViewById(R.id.channel_edit_ok_button).setOnClickListener(this);
+        findViewById(R.id.channel_edit_cancel_button).setOnClickListener(this);
+        findViewById(R.id.channel_edit_try_play_button).setOnClickListener(this);
+        findViewById(R.id.channel_edit_remove_button).setOnClickListener(this);
     }
 
     public void updateView() {
         updatePlayView();
         updateChannelsView();
+        updateChannelListView();
+        updateChannelEditView();
 
         if (dirty) {
             dirty = false;
@@ -332,40 +373,173 @@ public class MainActivity extends Activity
         }
     }
 
+    public ViewGroup newStarredChannelItem() {
+        LinearLayout item = new LinearLayout(this);
+        TextView star = new TextView(this);
+        star.setOnClickListener(this);
+        item.addView(star);
+        Button text = new Button(this);
+        text.setOnClickListener(this);
+        item.addView(text);
+        return item;
+    }
+
     public void updateChannelsView() {
         ViewGroup channelsView = (ViewGroup)findViewById(R.id.channels);
         int i = 0;
+        int j = 0;
         for (; i < channels.size(); ++i) {
             Channel channel = channels.get(i);
+            if (!channel.starred)
+                continue;
             if (i >= channelsView.getChildCount()) {
-                Button newButton = new Button(this);
-                newButton.setOnClickListener(this);
-                channelsView.addView(newButton);
+                channelsView.addView(newStarredChannelItem());
             }
-            Button chButton = (Button)channelsView.getChildAt(i);
-            if (chButton == clickedView) {
+            ViewGroup starredItem = (ViewGroup)channelsView.getChildAt(j);
+            TextView starText = (TextView)starredItem.getChildAt(0);
+            starText.setText(R.string.starred);
+            Button channelText = (Button)starredItem.getChildAt(1);
+            if (channelText == clickedView) {
                 clickedView = null;
                 player.set(channel.name, channel.url);
                 player.play();
                 dirty = true;
             }
-            chButton.setText(channel.name);
+            channelText.setText(channel.name);
+            ++j;
         }
-        channelsView.removeViews(i, channelsView.getChildCount() - i);
+        channelsView.removeViews(j, channelsView.getChildCount() - j);
     }
 
-    public void addTestChannel(View view) {
-        Channel channel = new Channel();
-        channel.name = getString(R.string.ch_0_name);
-        channel.url = getString(R.string.ch_0_url);
-        channels.add(channel);
+    public ViewGroup newChannelItem() {
+        LinearLayout item = new LinearLayout(this);
+        Button star = new Button(this);
+        star.setOnClickListener(this);
+        item.addView(star);
+        Button text = new Button(this);
+        text.setOnClickListener(this);
+        item.addView(text);
+        Button edit = new Button(this);
+        edit.setOnClickListener(this);
+        item.addView(edit);
+        return item;
+    }
+
+    public void updateChannelListView() {
+        ViewGroup channelList = (ViewGroup)findViewById(R.id.channel_list);
+        int i = 0;
+        for (; i < channels.size(); ++i) {
+            Channel channel = channels.get(i);
+            if (i >= channelList.getChildCount()) {
+                channelList.addView(newChannelItem());
+            }
+            ViewGroup channelItem = (ViewGroup)channelList.getChildAt(i);
+
+            Button starButton = (Button)channelItem.getChildAt(0);
+            if (starButton == clickedView) {
+                clickedView = null;
+                channel.starred = channel.starred ? false : true;
+                dirty = true;
+            }
+            if (channel.starred) {
+                starButton.setText(R.string.starred);
+            } else {
+                starButton.setText(R.string.not_starred);
+            }
+
+            Button channelText = (Button)channelItem.getChildAt(1);
+            if (channelText == clickedView) {
+                clickedView = null;
+                player.set(channel.name, channel.url);
+                player.play();
+                dirty = true;
+            }
+            channelText.setText(channel.name);
+
+            Button editButton = (Button)channelItem.getChildAt(2);
+            if (editButton == clickedView) {
+                clickedView = null;
+                editChannel(channel);
+                dirty = true;
+            }
+            editButton.setText(getString(R.string.edit));
+        }
+        channelList.removeViews(i, channelList.getChildCount() - i);
+    }
+
+    public void updateChannelEditView() {
+        Button ok = (Button)findViewById(R.id.channel_edit_ok_button);
+        EditText nameInput = (EditText)findViewById(R.id.channel_edit_name_input);
+        EditText urlInput = (EditText)findViewById(R.id.channel_edit_url_input);
+
+        Button cancel = (Button)findViewById(R.id.channel_edit_cancel_button);
+        Button tryPlayButton = (Button)findViewById(R.id.channel_edit_try_play_button);
+        Button removeButton = (Button)findViewById(R.id.channel_edit_remove_button);
+        removeButton.setVisibility((targetChannel == newChannel) ? View.GONE : View.VISIBLE);
+        String name = nameInput.getText().toString();
+        String url = urlInput.getText().toString().trim();
+
+        if (ok == clickedView) {
+            editedChannel.name = name;
+            editedChannel.url = url;
+
+            clickedView = null;
+            targetChannel.assign(editedChannel);
+            if (targetChannel == newChannel) {
+                channels.add(new Channel(newChannel));
+            } else {
+            }
+            changeView(findViewById(R.id.channel_list_view));
+            dirty = true;
+        }
+        if (cancel == clickedView) {
+            clickedView = null;
+            changeView(findViewById(R.id.channel_list_view));
+            dirty = true;
+        }
+        if (tryPlayButton == clickedView) {
+            clickedView = null;
+            player.set(name, url);
+            player.play();
+            dirty = true;
+        }
+        if (removeButton == clickedView) {
+            clickedView = null;
+            if (targetChannel != newChannel) {
+                channels.remove(targetChannel);
+            }
+            changeView(findViewById(R.id.channel_list_view));
+            dirty = true;
+        }
+    }
+
+    public void editChannel(Channel channel) {
+        targetChannel = channel;
+        editedChannel.assign(channel);
+        EditText nameInput = (EditText)findViewById(R.id.channel_edit_name_input);
+        nameInput.setText(editedChannel.name);
+        EditText urlInput = (EditText)findViewById(R.id.channel_edit_url_input);
+        urlInput.setText(editedChannel.url);
+        changeView(findViewById(R.id.channel_edit_view));
+    }
+
+    public void changeView(View view) {
+        findViewById(R.id.main_view).setVisibility(View.GONE);
+        findViewById(R.id.channel_list_view).setVisibility(View.GONE);
+        findViewById(R.id.channel_edit_view).setVisibility(View.GONE);
+        view.setVisibility(View.VISIBLE);
+    }
+
+    public void onMoreChannels(View view) {
+        changeView(findViewById(R.id.channel_list_view));
         updateView();
     }
 
-    public void removeLastChannel(View view) {
-        if (channels.size() > 0) {
-            channels.remove(channels.size() - 1);
-        }
+    public void onAddChannel(View view) {
+        newChannel.name = "";
+        newChannel.url = "";
+        newChannel.starred = false;
+        editChannel(newChannel);
         updateView();
     }
 
